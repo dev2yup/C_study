e51f4a8467f79bb81b3f0fa7060d66686017239f
dev2yup <leedy5521@naver.com>
2022-04-02 01:32:12 +0900

    공부

--- baekjoonTest.c
+++ baekjoonTest.c
@@ -3,18 +3,24 @@
 int main()
 
 {
-    int A, B;
-    scanf("%d %d", &A, &B);
-
-    if(A > B){
-        printf(">");
+    int score, test;
+    scanf("%d", &score);
+    if(score >= 90){
+        printf("A");
     }
-    else if(A < B){
-        printf("<");
+    else if(score >= 80){
+        printf("B");
+    }
+    else if(score >= 70){
+        printf("C");
+    }
+    else if(score >= 60){
+        printf("D");
     }
     else{
-        printf("==");
+        printf("F");
     }
+
     return 0;
 }
 

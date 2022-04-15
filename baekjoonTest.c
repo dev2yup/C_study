2d9a8bb2ddfe079828b1129be1e82b8b59d56b9a
dev2yup <leedy5521@naver.com>
2022-04-16 03:08:14 +0900

    study

--- baekjoonTest.c
+++ baekjoonTest.c
@@ -1,21 +1,44 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
+// int main()
 
-{
-    int year;
-    scanf("%d", &year);
-    if(year % 400 == 0)
-    {
-        printf("1");
-    }
-    else if(year % 100 == 0)
-    {
-        printf("0");
-    }
-    else if(year % 4 == 0)
-        printf("1");
-    else
-        printf("0");
-    return 0;
-} //윤년 판별
+// {
+//     int year;
+//     scanf("%d", &year);
+//     if(year % 400 == 0)
+//     {
+//         printf("1");
+//     }
+//     else if(year % 100 == 0)
+//     {
+//         printf("0");
+//     }
+//     else if(year % 4 == 0)
+//         printf("1");
+//     else
+//         printf("0");
+//     return 0;
+// } //윤년 판별
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int hour, minute;
+//     scanf("%d %d", &hour, &minute);
+//     if(hour == 0 && minute < 45)
+//         printf("%d %d", hour + 23, minute + 15);
+//     else if(hour == 0 && minute >= 45)
+//         printf("%d %d", hour = 0, minute -45);
+//     else if(minute < 45)
+//      {   
+//         minute = minute + 15;
+//         printf("%d %d", --hour, minute);
+//      }
+//     else
+//     {
+//        printf("%d %d", hour, minute - 45);
+//     }
+
+//     return 0;
+// } //시계 45분 빠르게(24시간제)

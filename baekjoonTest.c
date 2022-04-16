9c90f22334ef02a54553f29bccb8592f8c043fec
dev2yup <leedy5521@naver.com>
2022-04-17 02:46:08 +0900

    baekjoon

--- baekjoonTest.c
+++ baekjoonTest.c
@@ -42,3 +42,46 @@
 
 //     return 0;
 // } //시계 45분 빠르게(24시간제)
+
+#include <stdio.h>
+
+int main(void){
+
+    int hour, minute, makingtime, total;
+    scanf("%d %d", &hour, &minute);
+    scanf("%d", &makingtime);
+    total = minute + makingtime;
+    
+    if(hour == 23 && minute + makingtime >= 60)
+    {
+        hour += total / 60;
+        total = total - 60 * (total / 60);
+        if(hour >= 24)
+        {
+            printf("%d %d", 0 + hour - 24, total);
+        }
+        else
+        {
+            printf("%d %d", hour, total);
+        }
+    }
+    else if(total >= 60)
+    {
+        hour += total / 60;
+        total = total - 60 * (total / 60);
+        if(hour >= 24)
+        {
+            printf("%d %d", 0 + hour - 24, total);
+        }
+        else
+        {
+            printf("%d %d", hour, total);
+        }
+    }
+    else
+    {
+        printf("%d %d", hour, total);
+    }
+
+    return 0;
+}
